b3dfc69b85cddfe40e13a2aafab1bd55b3d82463
Ivan-Pavlukhin <pavlukhinivan@gmail.com>
2021-07-02 13:44:06 +0300

    Added try to  async/await

--- src/views/MovieDetailsView.js
+++ src/views/MovieDetailsView.js
@@ -16,12 +16,22 @@ const POSTER_URL = 'https://image.tmdb.org/t/p/original'
     release_date: null,
     vote_average: null,
     genres: null,
+    error: null,
+    loading: false,
     
   };
-  async componentDidMount() {
-    const { movieId } = this.props.match.params;
-    const response = await fetchMovieId(movieId);
-    this.setState({ ...response.data });
+   async componentDidMount() {
+     this.setState({loading: true})
+     try {
+      const { movieId } = this.props.match.params;
+      const response = await fetchMovieId(movieId);
+      this.setState({ ...response.data });
+     } catch(error) {
+       this.setState({error: error})
+     } finally {
+       this.setState({loading: false})
+     }
+    
    }
    
    handelBack = () => {
@@ -36,51 +46,52 @@ const POSTER_URL = 'https://image.tmdb.org/t/p/original'
    }
 
    render() {
-     const { title, release_date, vote_average, overview, genres } = this.state
+     const { title, release_date, vote_average, overview, genres, loading } = this.state
      const {location, match} = this.props
     return (
         <>
           <button type="button" onClick={this.handelBack}>&larr; To back</button>
           <div className={style.container}>
+            {loading && <p>Загружаем</p>}
             <img className={style.container__item}
                 src={`${POSTER_URL}${this.state.poster_path}`}
                 width="480"
                 alt="poster"
             />
-          <div
-          className={style.container__item}>
-                    <h1>
-                    {title} ({release_date})
-                    </h1>
-                    <p>User score: {vote_average * 10}%</p>
-                    <h2>Overview</h2>
-                    <p>{overview}</p>
-                    <h2>Genres </h2>
-                    <ul>
-                    {genres &&
-                        genres.map(genre => <li key={genre.name}>{genre.name}</li>)}
-                    </ul>
-                </div>
+            <div
+              className={style.container__item}>
+                <h1>
+                {title} ({release_date})
+                </h1>
+                <p>User score: {vote_average * 10}%</p>
+                <h2>Overview</h2>
+                <p>{overview}</p>
+                <h2>Genres </h2>
+                <ul>
+                  {genres &&
+                    genres.map(genre => <li key={genre.name}>{genre.name}</li>)}
+                </ul>
             </div>
-            <div>
-              <p>Additional information</p>
-              <ul>
-                  <li>
-                    <Link to={{
-                      pathname: `${match.url}/credits`,
-                      state: { from: location }
-                    }}  >Cast</Link>
-                  </li>
-                  <li>
-                    <Link to={{
-                      pathname: `${match.url}/reviews`,
-                      state: { from: location }
-                    }}>Reviews</Link>
-                  </li>
-              </ul>
-              <Route path={`${match.path}/credits`} component={Cast} />
-              <Route path={`${match.path}/reviews`} component={Reviews} />
-            </div>
+          </div>
+          <div>
+            <p>Additional information</p>
+            <ul>
+                <li>
+                  <Link to={{
+                    pathname: `${match.url}/credits`,
+                    state: { from: location }
+                  }}  >Cast</Link>
+                </li>
+                <li>
+                  <Link to={{
+                    pathname: `${match.url}/reviews`,
+                    state: { from: location }
+                  }}>Reviews</Link>
+                </li>
+            </ul>
+            <Route path={`${match.path}/credits`} component={Cast} />
+            <Route path={`${match.path}/reviews`} component={Reviews} />
+          </div>
         </>
     );
   }
