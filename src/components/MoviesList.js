95a7471c720ccb7af1b02d7a0708d810c29d44ef
Ivan-Pavlukhin <pavlukhinivan@gmail.com>
2021-07-03 20:24:50 +0300

    Added button go back to search list films

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,19 +1,23 @@
 
 import { NavLink, withRouter } from "react-router-dom";
 
-const MoviesList = ({ movies, location }) => {
-        return (
-            <ul>
-                {console.log(location)}
-                {movies && movies.map(movie => <li key={movie.id}>
-                    <NavLink to={{
-                        pathname: `/movies/${movie.id}`,
-                        state: { from: location }
-                    }}>
-                        {movie.title}
-                    </NavLink>
-                </li>)}
-            </ul>)
+const MoviesList = ({movies, location}) => {
+
+    return (
+            <>
+                <p>movies list</p>
+                <ul>
+                    {console.log(location)}
+                    {movies && movies.map(movie => <li key={movie.id}>
+                        <NavLink to={{
+                            pathname: `/movies/${movie.id}`,
+                            state: { from: location }
+                        }}>
+                            {movie.title}
+                        </NavLink>
+                    </li>)}
+                </ul>
+            </>)
 }
 
 export default withRouter(MoviesList)
--- src/views/MoviesView.js
+++ src/views/MoviesView.js
@@ -1,6 +1,8 @@
+import queryString from 'query-string';
 import { Component } from "react";
 import { fetchMoviesQuery } from "../service/service";
 import MoviesList from "../components/MoviesList";
+import { NavLink, Route } from "react-router-dom";
 
 class MoviesView extends Component {
     state = {
@@ -10,31 +12,51 @@ class MoviesView extends Component {
         error: null
     }
 
+    
+
+    componentDidMount() {
+        // const paramsString = "q=URLUtils.searchParams&topic=api";
+        // class searchParams = new URLSearchParams(paramsString);
+        const queryParams = queryString.parse(this.props.location.search);
+        console.log(queryParams)
+        if (this.props.location.search) {
+            this.setState({value: queryParams.query})
+            // console.log(this.props.location.search)
+            this.fetchMovies(queryParams.query)
+        }
+    }
+
     handelChange = (e) => {
         e.preventDefault();
         this.setState({value: e.currentTarget.value})
     }
 
      handelSubmit = (e) => {
-         e.preventDefault()
-         this.setState({loading: true})
-         fetchMoviesQuery(this.state.value)
+         if (this.state.value) {
+             this.setState({ loading: true })
+             this.fetchMovies(this.state.value)
+         }
+        
+    }
+    fetchMovies = (query) => {
+        return fetchMoviesQuery(query)
             .then(res => this.setState({ movies: res.data.results }))
             .catch(error => this.setState({ error: error }))
             .finally(() => this.setState({loading: false}))
-        
     }
 
     render() {
-        const {movies, loading} = this.state
+        const {movies, loading, value} = this.state
         return (
             <>
-                <form onSubmit={this.handelSubmit}>
-                    <input type="text" onChange={this.handelChange}/>
-                    <button type="submit">Search</button>
-                </form>
+                {/* {console.log(this.props.match.url)} */}
+                <input type="text" onChange={this.handelChange}/>
+                <NavLink to={`${this.props.match.url}?query=${this.state.value}`}
+                onClick={this.handelSubmit}>Search</NavLink>
                 {loading && <p>Загружаем</p>}
-                {movies && <MoviesList movies={movies} />}
+                {/* {<Route path={`/movies?query=${this.state.value}`}
+                    render={(props) => <MoviesList {...props} movies={this.state.movies} />} />} */}
+                {movies && <MoviesList movies={movies} query={value}/>}
             </>
         )
     }
